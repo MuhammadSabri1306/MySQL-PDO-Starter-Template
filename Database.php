087cfb5bbb6665335470747c0b66f23b9ca568d9
MuhammadSabri1306 <94425099+MuhammadSabri1306@users.noreply.github.com>
2022-03-23 11:33:06 +0700

    Update Database.php

--- Database.php
+++ Database.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * MySQL PDO Starter Template for PHP
- * source: https://github.com/MuhammadSabri1306/MySQL-PDO-Starter-Template
- * By Muhammad Sabri
+ * @source https://github.com/MuhammadSabri1306/MySQL-PDO-Starter-Template
+ * @author Muhammad Sabri <muhammadsabri1306@gmail.com>
  *
  */
 class Database
@@ -28,8 +28,12 @@ class Database
 		}
 	}
 
+	/**
+	 * Setup the MySQL's query of for operation
+	 * @param String $query
+	 */
 	function query($query){
-		$this->stm =$this->db->prepare($query);
+		$this->stm = $this->db->prepare($query);
 	}
 
 	function bind($param, $value, $type = null){
@@ -43,17 +47,53 @@ class Database
 		$this->stm->bindValue($param, $value, $type);
 	}
 
+	/**
+	 * Execute MySQL query after set
+	 */
 	function execute(){
-		$this->stm->execute();
+		if($this->stm->execute()){
+			return true;
+		}
+		return false;
 	}
 
+	/**
+	 * Get result as table by operation, used with SELECT query
+	 * @return (Multidimensional) Array
+	 * @example $data = $db->resultSet(); echo $data[0]['field'];
+	 */
 	function resultSet(){
 		$this->execute();
 		return $this->stm->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	/**
+	 * Get result as a row by operation, used with SELECT query
+	 * @return Array
+	 * @example $data = $db->resultRow(); echo $data['field'];
+	 */
 	function resultRow(){
 		$this->execute();
 		return $this->stm->fetch(PDO::FETCH_ASSOC);
+	}
+
+	/**
+	 * Get number of rows by operation's result, used with SELECT query
+	 * @return Number
+	 */
+	function numRows(){
+		return count($this->resultSet());
+	}
+
+	/**
+	 * Get id recently inserted, used after INSERT query
+	 * @return String
+	 */
+	function lastInsertId(){
+		return $this->db->lastInsertId();
+	}
+
+	function debugDumpParams(){
+		return $this->stm->debugDumpParams();
 	}
 }
